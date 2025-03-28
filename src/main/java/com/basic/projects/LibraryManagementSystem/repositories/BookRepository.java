5dc3f9f7346e14110bb6d58c0fe95e62bffaf7cb
arjunjagwani <arjunjagwani6@gmail.com>
2025-03-28 14:10:13 +0530

    Added All the functionalities

--- src/main/java/com/basic/projects/LibraryManagementSystem/repositories/BookRepository.java
+++ src/main/java/com/basic/projects/LibraryManagementSystem/repositories/BookRepository.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
 
+    List<Book> findByTitleContainingIgnoreCase(String query);
 }
--- src/main/java/com/basic/projects/LibraryManagementSystem/services/BookService.java
+++ src/main/java/com/basic/projects/LibraryManagementSystem/services/BookService.java
@@ -5,7 +5,9 @@ import com.basic.projects.LibraryManagementSystem.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,11 +37,21 @@ public class BookService {
     }
 
     public void updateNewBook(long id, String title, String author, String genre, boolean availStatus) {
-        Book book =bookRepository.findById(id).orElse(null);
+        Book book = bookRepository.findById(id).orElse(null);
         book.setTitle(title);
         book.setAuthor(author);
         book.setGenre(genre);
         book.setAvail_status(availStatus);
         bookRepository.save(book);
+    }
+
+    public List<Book> searchBooks(String query) {
+
+        if (query.matches("\\d+")) { // If the input is a number, assume it's a Book ID
+            Optional<Book> book = bookRepository.findById(Long.parseLong(query));
+            return book.map(Collections::singletonList).orElse(Collections.emptyList());
+        } else { // Otherwise, assume it's a Title search
+            return bookRepository.findByTitleContainingIgnoreCase(query);
+        }
     }
 }
